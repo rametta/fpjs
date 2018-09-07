bf2957a45bd2de6ca82e0945e8bf0e0291a1c186
Jason <jrametta@dynamite.ca>
2018-09-07 11:31:26 -0400

    added sanctuary def to environment

--- src/app.redux.js
+++ src/app.redux.js
@@ -3,6 +3,7 @@ import * as L from 'partial.lenses'
 import * as R from 'ramda'
 import daggy from 'daggy'
 import stringify from 'json-stringify'
+import * as $ from 'sanctuary-def'
 import { create, env } from 'sanctuary'
 const S = create({ checkTypes: true, env })
 const { encaseEither, either, Left, Right, pipeK, compose } = S
@@ -10,86 +11,66 @@ const { encaseEither, either, Left, Right, pipeK, compose } = S
 const Status = daggy.taggedSum('Status', {
   Success: ['result'],
   Error: ['msg'],
-  Empty: []
+  Empty: [],
 })
 
 const types = {
   SET_EDITOR: 'SET_EDITOR',
-  EXECUTE: 'EXECUTE'
+  EXECUTE: 'EXECUTE',
 }
 
 const initial = {
   editor: 'S.compose (Math.sqrt) (S.add (1)) (8)',
-  status: Status.Empty
+  status: Status.Empty,
 }
 
 // setEditor :: Payload -> Action
-export const setEditor = (payload) => ({ type: types.SET_EDITOR, payload })
+export const setEditor = payload => ({ type: types.SET_EDITOR, payload })
 
 // execute :: Action
 export const execute = () => ({ type: types.EXECUTE })
 
 // isEmpty :: String -> Either
-const isEmpty = (code) =>
-  code ? Right(code) : Left('🥛 Empty: type something in...')
+const isEmpty = code => (code ? Right(code) : Left('🥛 Empty: type something in...'))
 
 // safeStringify :: String -> Either
-const safeStringify = (result) =>
-  encaseEither(() => '🚨 99 problems and your code is all of them')((r) =>
-    stringify(r, null, 2)
-  )(result)
+const safeStringify = result =>
+  encaseEither(() => '🚨 99 problems and your code is all of them')(r => stringify(r, null, 2))(result)
 
 // isObject :: Result -> Either
-const isObject = (result) =>
-  result !== null && typeof result === 'object'
-    ? safeStringify(result)
-    : Right(result)
+const isObject = result => (result !== null && typeof result === 'object' ? safeStringify(result) : Right(result))
 
 // isBool :: Result -> Either
-const isBool = (result) =>
-  typeof result === 'boolean' ? Right(JSON.stringify(result)) : Right(result)
+const isBool = result => (typeof result === 'boolean' ? Right(JSON.stringify(result)) : Right(result))
 
 // isFunction :: Result -> Either
-const isFunction = (result) =>
-  typeof result === 'function' ? Right(result.toString()) : Right(result)
+const isFunction = result => (typeof result === 'function' ? Right(result.toString()) : Right(result))
 
 // isArray :: Result -> Either
-const isArray = (result) =>
-  result.constructor === Array ? Right(JSON.stringify(result)) : Right(result)
+const isArray = result => (result.constructor === Array ? Right(JSON.stringify(result)) : Right(result))
 
 // hasResult :: Result -> Either
-const hasResult = (result) => (result ? Right(result) : Left('🌌 VOID'))
+const hasResult = result => (result ? Right(result) : Left('🌌 VOID'))
 
 // withContext :: String -> Throwable String
-const withContext = (code) =>
-  evalInContext.call({ S, L, R, daggy, redux, code })
+const withContext = code => evalInContext.call({ S, L, R, $, daggy, redux, code })
 
 // tryEval :: String -> Either
-const tryEval = encaseEither((e) => e.toString())(withContext)
+const tryEval = encaseEither(e => e.toString())(withContext)
 
 // log :: Result -> Either
-const log = (result) => {
+const log = result => {
   console.info(result)
   return Right(result)
 }
 
-const run = compose(
-  pipeK([
-    isEmpty,
-    tryEval,
-    log,
-    isBool,
-    hasResult,
-    isArray,
-    isFunction,
-    isObject
-  ])
-)(Right)
+const run = compose(pipeK([isEmpty, tryEval, log, isBool, hasResult, isArray, isFunction, isObject]))(Right)
 
 // evalInContext :: String
 function evalInContext() {
   // eslint-disable-next-line
   return eval(`
+    const $ = this.$
     const S = this.S
     const L = this.L
     const R = this.R
@@ -104,15 +85,13 @@ export const reducer = (state = initial, { type, payload }) => {
     case types.SET_EDITOR:
       return {
         ...state,
-        editor: payload
+        editor: payload,
       }
     case types.EXECUTE:
-      return either((error) => ({ ...state, status: Status.Error(error) }))(
-        (result) => ({
-          ...state,
-          status: Status.Success(result)
-        })
-      )(run(state.editor))
+      return either(error => ({ ...state, status: Status.Error(error) }))(result => ({
+        ...state,
+        status: Status.Success(result),
+      }))(run(state.editor))
     default:
       return state
   }
